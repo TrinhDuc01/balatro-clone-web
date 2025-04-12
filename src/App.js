0ed734b1370f2c5992515c25e9d9385f0d22c081
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-12 20:22:50 +0700

    test background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 
 import handRanking from './Class/HandRankingOption';
 import PlayCard from './Class/PlayCard';
+import BackgroundCanvas from './Components/Background/BackgroundCanvas';
+import Card from './Components/Card';
 
 function App() {
 
   console.log(handRanking([
-    new PlayCard('2', 'spread'),
-    new PlayCard('5', 'spread'),
+    new PlayCard('Ace', 'spread'),
+    new PlayCard('5', 'Club'),
     new PlayCard('3', 'spread'),
     new PlayCard('4', 'spread'),
     new PlayCard('Ace', 'spread'),
   ]))
   return (
-    <div className="App">
-
+    <div className="App" style={{backgroundColor:"black",width:'100vw',height:'100vh'}}>
+      {/* <Card/> */}
+      <BackgroundCanvas/>
     </div>
   );
 }
