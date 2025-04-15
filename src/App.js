d31f2abe96cbbb7460e4fe2289387ac33b60ff69
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-15 15:39:46 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 import handRanking from './Class/HandRankingOption';
 import PlayCard from './Class/PlayCard';
-import BackgroundCanvas from './Components/Background/BackgroundCanvas';
-import Card from './Components/Card';
+import BackgroundHomeCanvas from './Components/BackgroundHome/BackgroundHomeCanvas';
+import ButtonToggleMusic from './Components/BackgroundMusic/ButtonToggleMusic';
+import Card from './Components/Card/Card';
+import Deck from './Components/Deck/Deck';
+import Combine from './Components/MainGame/Combine/Combine';
 
 function App() {
 
@@ -13,10 +16,17 @@ function App() {
     new PlayCard('4', 'spread'),
     new PlayCard('Ace', 'spread'),
   ]))
+  console.log(Deck())
   return (
-    <div className="App" style={{backgroundColor:"black",width:'100vw',height:'100vh'}}>
-      {/* <Card/> */}
-      <BackgroundCanvas/>
+    <div className="App" style={{ backgroundColor: "black", width: '100vw', height: '100vh' }}>
+
+
+      {/* <Card margin={0}/>
+      <Card margin={50}/>
+      <Card margin={100}/> */}
+        <Combine/>
+      <BackgroundHomeCanvas/>
+      <ButtonToggleMusic />
     </div>
   );
 }
