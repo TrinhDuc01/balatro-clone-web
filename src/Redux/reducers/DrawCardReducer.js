574e3ab94eede1a044183efb6034917fe8a990ac
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-17 01:55:41 +0700

    Tạo reducer để bốc bài

---
+++ src/Redux/reducers/DrawCardReducer.js
@@ -0,0 +1,37 @@
+const defaultState = {
+    remainingCards: [],
+    drawCard: []
+}
+
+const DrawCardReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "InitDeckRoot":
+            return {
+                remainingCards: [...action.payload],
+                drawCard: []
+            }
+        case "Draw": {
+            const shuffledDeck = [...action.deck]; // Tạo bản sao mảng để không làm thay đổi mảng gốc
+            const drawnCards = [];
+            // Xáo trộn mảng
+            for (let i = shuffledDeck.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+            }
+            // Lấy num phần tử ngẫu nhiên
+            for (let i = 0; i < action.num; i++) {
+                drawnCards.push(shuffledDeck.pop());
+            }
+
+            return {
+                remainingCards: shuffledDeck,
+                drawCard: drawnCards
+            }
+        }
+
+        default:
+            return state
+    }
+}
+
+export default DrawCardReducer
