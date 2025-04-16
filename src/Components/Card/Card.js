5a8ab942e905e8dce9166f56b43f4f5231757123
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-17 01:58:39 +0700

    Sửa 1 số thứ cho card và thêm hiệu ứng hover thì card sẽ nghiêng

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,8 +1,27 @@
-import "./test.css"
+import { useState } from "react";
+import "./card.css"
 
-const Card = ({margin}) => {
-    return <div className="test" style={{marginLeft:margin}}>
-        
+const Card = ({ margin }) => {
+    const [transformStyle, setTransformStyle] = useState('rotateX(0deg) rotateY(0deg)');
+
+    const handleMouseMove = (e) => {
+        const card = e.target.getBoundingClientRect();
+        const x = e.clientX - card.left;
+        const y = e.clientY - card.top;
+
+        const centerX = card.width / 2;
+        const centerY = card.height / 2;
+
+        const rotateX = ((y - centerY) / centerY) * 30; // độ nghiêng trục X
+        const rotateY = ((x - centerX) / centerX) * 30; // độ nghiêng trục Y
+
+        setTransformStyle(`perspective(400px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`);
+    }
+    return <div
+        onMouseLeave={(e)=> setTransformStyle()}
+        onMouseMove={(e) => handleMouseMove(e)}
+        style={{ transform: transformStyle,left:margin }} className="card ">
+
     </div>
 }
 
