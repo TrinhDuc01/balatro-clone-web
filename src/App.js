ba7423fdd09348eb8e0bdb0ce917d1a7d3187349
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-17 02:23:19 +0700

    Triển khai test chức năng bốc bài

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useEffect } from 'react';
 import handRanking from './Class/HandRankingOption';
 import PlayCard from './Class/PlayCard';
 import BackgroundHomeCanvas from './Components/BackgroundHome/BackgroundHomeCanvas';
@@ -6,24 +7,54 @@ import ButtonToggleMusic from './Components/BackgroundMusic/ButtonToggleMusic';
 import Card from './Components/Card/Card';
 import Deck from './Components/Deck/Deck';
 import Combine from './Components/MainGame/Combine/Combine';
+import { useDispatch, useSelector } from 'react-redux';
+import DrawCardReducer from './Redux/reducers/DrawCardReducer';
 
 function App() {
 
-  console.log(handRanking([
-    new PlayCard('Ace', 'spread'),
-    new PlayCard('5', 'Club'),
-    new PlayCard('3', 'spread'),
-    new PlayCard('4', 'spread'),
-    new PlayCard('Ace', 'spread'),
-  ]))
-  console.log(Deck())
+  // console.log(handRanking([
+  //   new PlayCard('Ace', 'spread'),
+  //   new PlayCard('5', 'Club'),
+  //   new PlayCard('3', 'spread'),
+  //   new PlayCard('4', 'spread'),
+  //   new PlayCard('Ace', 'spread'),
+  // ]))
+  // console.log(Deck())
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch({ type: 'InitDeckRoot', payload: Deck() })
+  }, [])
+
+  const drawCard = useSelector((state) => {
+    console.log(state.DrawCardReducer)
+    return state.DrawCardReducer.drawCard
+  })
+
+  const remainingCards = useSelector((state) => {
+    return state.DrawCardReducer.remainingCards
+  })
+
+
+  const handleDrawCard = () => {
+    dispatch({
+      type: "Draw",
+      deck: remainingCards,
+      num: 8,
+
+    })
+  }
+
+
   return (
     <div className="App" style={{ backgroundColor: "black", width: '100vw', height: '100vh' }}>
+      {drawCard&&drawCard.map((ele,index)=>{
+        return <Card key={index} left={index*40} position={ele.position}>
 
-
-      <Card margin={0} />
-      {/* <Card margin={50} />
-      <Card margin={100} /> */}
+        </Card>
+      })}
+      <div onClick={handleDrawCard} style={{ position: 'absolute', zIndex: 10000, backgroundColor: 'red' }}>
+        Rut bai
+      </div>
       <Combine />
       <BackgroundHomeCanvas />
       <ButtonToggleMusic />
