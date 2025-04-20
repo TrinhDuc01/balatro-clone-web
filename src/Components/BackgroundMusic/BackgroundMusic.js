c3ac0113f7cc3f8d0323f8254df83b5457d7ca12
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-20 22:47:11 +0700

    giảm volume và thêm tải trước để tránh lag

--- src/Components/BackgroundMusic/BackgroundMusic.js
+++ src/Components/BackgroundMusic/BackgroundMusic.js
@@ -1,8 +1,13 @@
+import { useEffect } from "react"
 
 
 const BackgroundMusic = () => {
+    useEffect(() => {
+        document.getElementById("background-audio").volume = 0.4
+    })
+
     return <div>
-        <audio src="./Sounds/01. Main Theme.mp3" autoPlay loop></audio>
+        <audio preload="auto" id="background-audio" src="./Sounds/01. Main Theme.mp3" volume autoPlay loop></audio>
     </div>
 }
 
