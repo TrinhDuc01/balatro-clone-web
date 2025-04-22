ebef6277f279bb907840079b09a8a2168193bdf9
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-04-22 21:07:49 +0700

    loại bỏ phần thừa

--- src/Components/MainGame/SideBar/Round Score/RoundScore.js
+++ src/Components/MainGame/SideBar/Round Score/RoundScore.js
@@ -3,16 +3,7 @@ import "./roundScore.css"
 import { useEffect } from "react";
 
 const RoundScore = () => {
-    const dispatch = useDispatch()
-    const { chip, mult } = useSelector(state => state.ChipXMultReducer);
     const { roundScore } = useSelector(state => state.RoundScoreReducer);
-    // useEffect(() => {
-    //     console.log(chip,mult,'hehe')
-    //     dispatch({
-    //         type:"IncreaseRoundScore",
-    //         payload: chip*mult
-    //     })
-    // }, [dispatch])
     return <div className="round-score pixel-corners">
         <div className="title">
             <div>Round</div>
